2d283ca4f8ad54e9bf51201292a0f2d360d9421c
v1ztep <v1ztep@gmail.com>
2021-10-05 21:55:00 +0300

    Добавил управление кораблю

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import random
 import time
 from itertools import cycle
 
+SPACE_KEY_CODE = 32
+LEFT_KEY_CODE = 260
+RIGHT_KEY_CODE = 261
+UP_KEY_CODE = 259
+DOWN_KEY_CODE = 258
+
 
 def draw(canvas):
     curses.curs_set(False)
     canvas.border()
+    canvas.nodelay(True)
 
     rows = canvas.getmaxyx()[0] - 2
     columns = canvas.getmaxyx()[1] - 2
@@ -99,6 +106,34 @@ async def animate_spaceship(canvas, rows, columns):
             canvas, start_row, start_column, frame, negative=True
         )
 
+        rows_direction, columns_direction, space_pressed = read_controls(canvas)
+        start_row += rows_direction
+        start_column += columns_direction
+
+
+def read_controls(canvas):
+    rows_direction = columns_direction = 0
+    space_pressed = False
+
+    while True:
+        pressed_key_code = canvas.getch()
+
+        if pressed_key_code == -1:
+            break
+
+        if pressed_key_code == UP_KEY_CODE:
+            rows_direction = -1
+        if pressed_key_code == DOWN_KEY_CODE:
+            rows_direction = 1
+        if pressed_key_code == RIGHT_KEY_CODE:
+            columns_direction = 1
+        if pressed_key_code == LEFT_KEY_CODE:
+            columns_direction = -1
+        if pressed_key_code == SPACE_KEY_CODE:
+            space_pressed = True
+
+    return rows_direction, columns_direction, space_pressed
+
 
 def draw_frame(canvas, start_row, start_column, text, negative=False):
     rows_number, columns_number = canvas.getmaxyx()
@@ -106,20 +141,16 @@ def draw_frame(canvas, start_row, start_column, text, negative=False):
     for row, line in enumerate(text.splitlines(), round(start_row)):
         if row < 0:
             continue
-
         if row >= rows_number:
             break
 
         for column, symbol in enumerate(line, round(start_column)):
             if column < 0:
                 continue
-
             if column >= columns_number:
                 break
-
             if symbol == ' ':
                 continue
-
             if row == rows_number - 1 and column == columns_number - 1:
                 continue
 
