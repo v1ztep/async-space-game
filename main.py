8abd58d070ec01e69a76ea5ec75ad358974d13e9
v1ztep <v1ztep@gmail.com>
2021-10-19 19:40:56 +0300

    Вынес в отдельный модуль read_controls, draw_frame, get_frame_size

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import random
 import time
 from itertools import cycle
 from pathlib import Path
+
+from curses_tools import draw_frame
+from curses_tools import get_frame_size
+from curses_tools import read_controls
 from physics import update_speed
 
-SPACE_KEY_CODE = 32
-LEFT_KEY_CODE = 260
-RIGHT_KEY_CODE = 261
-UP_KEY_CODE = 259
-DOWN_KEY_CODE = 258
-
 COROUTINES = []
+OBSTACLES = []
 
 
 def draw(canvas):
@@ -162,60 +161,6 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
 async def sleep(tics=1):
     for _ in range(tics):
         await asyncio.sleep(0)
-
-
-def get_frame_size(text):
-    lines = text.splitlines()
-    rows = len(lines)
-    columns = max([len(line) for line in lines])
-    return rows, columns
-
-
-def read_controls(canvas):
-    rows_direction = columns_direction = 0
-    space_pressed = False
-
-    while True:
-        pressed_key_code = canvas.getch()
-
-        if pressed_key_code == -1:
-            break
-
-        if pressed_key_code == UP_KEY_CODE:
-            rows_direction = -1
-        if pressed_key_code == DOWN_KEY_CODE:
-            rows_direction = 1
-        if pressed_key_code == RIGHT_KEY_CODE:
-            columns_direction = 1
-        if pressed_key_code == LEFT_KEY_CODE:
-            columns_direction = -1
-        if pressed_key_code == SPACE_KEY_CODE:
-            space_pressed = True
-
-    return rows_direction, columns_direction, space_pressed
-
-
-def draw_frame(canvas, start_row, start_column, text, negative=False):
-    rows_number, columns_number = canvas.getmaxyx()
-
-    for row, line in enumerate(text.splitlines(), round(start_row)):
-        if row < 0:
-            continue
-        if row >= rows_number:
-            break
-
-        for column, symbol in enumerate(line, round(start_column)):
-            if column < 0:
-                continue
-            if column >= columns_number:
-                break
-            if symbol == ' ':
-                continue
-            if row == rows_number - 1 and column == columns_number - 1:
-                continue
-
-            symbol = symbol if not negative else ' '
-            canvas.addch(row, column, symbol)
 
 
 if __name__ == '__main__':
